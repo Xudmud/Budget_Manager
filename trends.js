9c3c7eb0732bf2ef5a63cda25755d87e651ce2f6
Xudmud <morichikarinnosuke17@gmail.com>
2019-04-04 16:09:21 -0600

    Basic trends.js file, a Spend and Trend schema.

--- trends.js
+++ trends.js
@@ -1,1 +1,31 @@
 /* trends.js file */
+/*  \users POST request financial spending data
+    \trends GET request to simply see the data that's been input */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB, {useNewUrlParser:true} );
+mongoose.set('useCreateIndex', true);
+
+var SpendSchema = new Schema({
+    amount: {type: Number, required: true},
+    category: {type: {String, enum}},
+    date: Date,
+})
+
+
+var TrendSchema = new Schema({
+    data: [{ type: Schema.Types.ObjectId, ref: 'Spend'}]
+})
+
+var Spend = mongoose.model('Spend',SpendSchema);
+var Trend = mongoose.model('Trend',TrendSchema);
+
+/* Measurement of financial information. */
+/*
+    amount: string,enum
+    category: (choose from list)
+    Date: Date
+*/
